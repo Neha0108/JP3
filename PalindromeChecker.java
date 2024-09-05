bfa51764ba01cdbbd4e3eb75f53547ddb73e6723
Neha0108 <112578569+Neha0108@users.noreply.github.com>
2024-09-05 10:11:53 +0530

    Update PalindromeChecker.java

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,19 +1,13 @@
-import java.util.Scanner;
-
 public class PalindromeChecker {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
+        // Hardcoded string
+        String input = "madam";  // You can change this string to any other string you want to check
 
         if (isPalindrome(input)) {
             System.out.println(input + " is a palindrome.");
         } else {
             System.out.println(input + " is not a palindrome.");
         }
-
-        scanner.close();
     }
 
     public static boolean isPalindrome(String str) {
